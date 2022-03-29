f5c761bd9b285be06b01c61fee5198aa00f1d93b
liwei <401613869@qq.com>
2022-03-29 20:41:45 +0800

    liwei

---
+++ src/cn/tedu/FileIODemo.java
@@ -0,0 +1,13 @@
+package cn.tedu;
+
+import java.io.File;
+
+/**
+ * @author liwei
+ * @create 2022-03-29-20:39
+ **/
+public class FileIODemo {
+    public static void main(String[] args) {
+        File file = new File("fos.txt");//
+    }
+}
